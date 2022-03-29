9e1017ed69ff906ca68c35b2d0b25b22fe665006
xiaoertang <1298840110@qq.com>
2022-03-29 16:37:30 +0800

    后端实现运营统计表

--- health_parent/health_backend/src/main/java/com/example/controller/ReportController.java
+++ health_parent/health_backend/src/main/java/com/example/controller/ReportController.java
@@ -6,10 +6,19 @@ import com.example.entity.Result;
 import com.example.service.MemberService;
 import com.example.service.ReportService;
 import com.example.service.SetMealService;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -29,6 +38,7 @@ public class ReportController {
     private SetMealService setMealService;
     @Reference
     private ReportService reportService;
+
     @GetMapping("/getMemberReport")
     public Result getMemberReport() {
         try {
@@ -54,7 +64,7 @@ public class ReportController {
 
     @GetMapping("/getSetmealReport")
     public Result getSetmealReport() {
-        try{
+        try {
             //存放饼图数据
             Map<String, Object> date = new HashMap<>();
             //饼图名称
@@ -69,19 +79,95 @@ public class ReportController {
             }
             date.put("setmealNames", setmealNames);
             return new Result(true, MessageConstant.GET_SETMEAL_COUNT_REPORT_SUCCESS, date);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return new Result(false, MessageConstant.GET_SETMEAL_COUNT_REPORT_FAIL);
         }
     }
+
     @GetMapping("/getBusinessReportData")
-    public Result getBusinessReportData(){
-        try{
+    public Result getBusinessReportData() {
+        try {
             Map<String, Object> businessReport = reportService.getBusinessReport();
-            return new Result(true,MessageConstant.GET_BUSINESS_REPORT_SUCCESS,businessReport);
+            return new Result(true, MessageConstant.GET_BUSINESS_REPORT_SUCCESS, businessReport);
         } catch (Exception e) {
             e.printStackTrace();
-            return new Result(false,MessageConstant.GET_BUSINESS_REPORT_FAIL);
+            return new Result(false, MessageConstant.GET_BUSINESS_REPORT_FAIL);
+        }
+    }
+
+    //导出Excel报表
+    @RequestMapping("/exportBusinessReport")
+    public Result exportBusinessReport(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            //远程调用报表服务获取报表数据
+            Map<String, Object> result = reportService.getBusinessReport();
+            //取出返回结果数据，准备将报表数据写入到Excel文件中
+            String reportDate = (String) result.get("reportDate");
+            Integer todayNewMember = (Integer) result.get("todayNewMember");
+            Integer totalMember = (Integer) result.get("totalMember");
+            Integer thisWeekNewMember = (Integer) result.get("thisWeekNewMember");
+            Integer thisMonthNewMember = (Integer) result.get("thisMonthNewMember");
+            Integer todayOrderNumber = (Integer) result.get("todayOrderNumber");
+            Integer thisWeekOrderNumber = (Integer) result.get("thisWeekOrderNumber");
+            Integer thisMonthOrderNumber = (Integer) result.get("thisMonthOrderNumber");
+            Integer todayVisitsNumber = (Integer) result.get("todayVisitsNumber");
+            Integer thisWeekVisitsNumber = (Integer) result.get("thisWeekVisitsNumber");
+            Integer thisMonthVisitsNumber = (Integer) result.get("thisMonthVisitsNumber");
+            List<Map> hotSetmeal = (List<Map>) result.get("hotSetmeal");
+
+            //获取Excel模板绝对路径
+            String filePath = request.getSession().getServletContext().getRealPath("template") + File.separator + "report_template.xlsx";
+            //读取模板文件创建Excel文件
+            XSSFWorkbook workbook = new XSSFWorkbook(filePath);
+            //获取第一张表
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            //输入单元格中的值
+            XSSFRow row = sheet.getRow(2);
+            row.getCell(5).setCellValue(reportDate);//日期
+
+            row = sheet.getRow(4);
+            row.getCell(5).setCellValue(todayNewMember);//新增会员数（本日）
+            row.getCell(7).setCellValue(totalMember);//总会员数
+
+            row = sheet.getRow(5);
+            row.getCell(5).setCellValue(thisWeekNewMember);//本周新增会员数
+            row.getCell(7).setCellValue(thisMonthNewMember);//本月新增会员数
+
+            row = sheet.getRow(7);
+            row.getCell(5).setCellValue(todayOrderNumber);//今日预约数
+            row.getCell(7).setCellValue(todayVisitsNumber);//今日到诊数
+
+            row = sheet.getRow(8);
+            row.getCell(5).setCellValue(thisWeekOrderNumber);//本周预约数
+            row.getCell(7).setCellValue(thisWeekVisitsNumber);//本周到诊数
+
+            row = sheet.getRow(9);
+            row.getCell(5).setCellValue(thisMonthOrderNumber);//本月预约数
+            row.getCell(7).setCellValue(thisMonthVisitsNumber);//本月到诊数
+            //获取行
+            int rowNum = 12;
+            for (Map map : hotSetmeal) {
+                String name = (String) map.get("name");
+                Long setmeal_count = (Long) map.get("setmeal_count");
+                BigDecimal proportion = (BigDecimal) map.get("proportion");
+                row = sheet.getRow(rowNum++);
+                row.getCell(4).setCellValue(name);//套餐名称
+                row.getCell(5).setCellValue(setmeal_count);//预约数量
+                row.getCell(6).setCellValue(proportion.doubleValue());//占比
+            }
+            //通过输出流进行下载
+            ServletOutputStream out = response.getOutputStream();
+            response.setContentType("application/vnd.ms-excel");
+            response.setHeader("content-Disposition","attachment;filename=repost.xlsx");
+            workbook.write(out);
+            out.flush();
+            out.close();
+            workbook.close();
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, MessageConstant.GET_BUSINESS_REPORT_FAIL, null);
         }
     }
 }
